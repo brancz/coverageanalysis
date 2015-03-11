c1d4d0cfa804b4874092cc5ef65128566c2cf1c7
flower-pot <flower-pot@flowerpot.io>
2015-03-11 12:44:48 +0100

    add json to coverage analysis

--- coverageanalysis.go
+++ coverageanalysis.go
@@ -1,5 +1,30 @@
 package main
 
+import (
+	"encoding/json"
+	"errors"
+)
+
 func main() {
 
+}
+
+func AnalyzeJson(jsonString string) (report *CoverageReport, err error) {
+	var jsonObject map[string]interface{}
+	err = json.Unmarshal([]byte(jsonString), &jsonObject)
+	if err != nil {
+		return &CoverageReport{}, err
+	}
+	defer func() { //catch or finally
+		if innerErr := recover(); innerErr != nil { //catch
+			err = errors.New("bad JSON")
+		}
+	}()
+	report = BuildCoverageReport(jsonObject)
+	AnalyzeReport(report)
+	return report, err
+}
+
+func AnalyzeReport(report *CoverageReport) {
+	CalculateAndSetAllFileCoverages(report)
 }
---
+++ coverageanalysis_test.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"errors"
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestJsonAnalysis(t *testing.T) {
+	report, err := AnalyzeJson(`{
+		"source_files": [{
+			"name": "test.rb",
+			"source": "puts 'test'",
+			"source_digest": "*md5*",
+			"coverage": [null, 1, null]
+		}]
+	}`)
+	if err != nil {
+		t.Error(err)
+	}
+	expectedReport := &CoverageReport{
+		CoveragePercentage: float64(1),
+		FileReports: []FileReport{
+			{
+				Name:               "test.rb",
+				FileSource:         "puts 'test'",
+				FileDigest:         "*md5*",
+				CoveragePercentage: float64(1),
+				LineCoverages: []LineCoverage{
+					{Hits: 0, Ignore: true},
+					{Hits: 1, Ignore: false},
+					{Hits: 0, Ignore: true}},
+			},
+		},
+	}
+	assert.Equal(t, report, expectedReport)
+}
+
+func TestJsonAnalysisWithMalformedJson(t *testing.T) {
+	_, err := AnalyzeJson("{")
+	assert.NotNil(t, err)
+}
+
+func TestJsonAnalysisWithWrongStructureOfJson(t *testing.T) {
+	_, err := AnalyzeJson("{}")
+	if assert.Error(t, err, "bad JSON") {
+		assert.Equal(t, err, errors.New("bad JSON"))
+	}
+}
